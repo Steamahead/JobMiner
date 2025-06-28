6f2c1b9f3847660ce0d8522639a5fa1ae7d379b6
Damian Sadza <sadza.damian@gmail.com>
2025-06-28 11:41:05 +0200

    Refactor JustJoin scraper location

--- JobScraper/justjoin-scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-import azure.functions as func
-from justjoin_scraper import JustJoinScraper
-
-def main(timer: func.TimerRequest):
-    scraper = JustJoinScraper()
-    scraper.scrape()
--- JobScraper/scrapers/__init__.py
+++ JobScraper/scrapers/__init__.py
@@ -1,1 +1,8 @@
+from .pracuj_scraper import PracujScraper, scrape_pracuj
+from .justjoin_scraper import JustJoinScraper
 
+__all__ = [
+    "PracujScraper",
+    "scrape_pracuj",
+    "JustJoinScraper",
+]
